4d627b5f9ada9c8524352f332eb4ea906198b6b6
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-19 22:13:40 +0200

    page d'incription, bouton d'inscription, début de bouton

--- View/MenuGauche.php
+++ View/MenuGauche.php
@@ -1,3 +1,5 @@
 <div class="menu-gauche">
     hihi pas de bouton :p 
+    <li <?php if ($_SERVER['PHP_SELF'] == "/BibliothequeAudio/view/playlist.php") echo "class='active'"; ?>><a href="playlist.php">Biblio </a></li>
+    <li <?php if ($_SERVER['PHP_SELF'] == "/BibliothequeAudio/view/Request_Audio.php") echo "class='active'"; ?>><a href="Request_Audio.php">Faire une demande</a></li>
 </div>
---
+++ View/NewUser.php
@@ -0,0 +1,28 @@
+<div class="nouveau">
+    <br/>
+    <br/>
+    <div class="milieuPage">
+        <div class="form-group">
+            <label for="contenu">Pseudo</label>
+            <input type="text" placeholder="Pseudo" id="Pseudo" class="form-control"/>
+        </div>
+
+        <div class="form-group">
+            <label for="contenu">Mot de passe</label>
+            <input type="password" placeholder="Password" id="Password" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label for="contenu">confirmer le mot de passe</label>
+            <input type="password" placeholder="Password" id="Password" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label for="contenu">Mail</label>
+            <input type="email" placeholder="Email" id="Email" class="form-control"/>
+        </div>
+        <div class="form-group">
+            <label for="contenu">Confirmer le mail</label>
+            <input type="email" placeholder="Email" id="Email" class="form-control"/>
+        </div>
+        <input type="button" class="btn btn-success" id="Sauvegarder" value="Sauvegarder"  />
+    </div>
+</div>
