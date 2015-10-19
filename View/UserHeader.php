5d1920ba3de8b6c2e3fea70ea16ea86b457f6ae6
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-19 19:13:11 +0200

    Positionnement du label user et du bouton déconnexion

--- View/UserHeader.php
+++ View/UserHeader.php
@@ -1,19 +1,21 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: anthony
+ * User: anthony et arnaud
  * Date: 13/10/2015
  * Time: 20:14
  */
 ?>
-    <nav class="navbar navbar-default"">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    <img alt="Bibliothèque Audio V2" src="...">
-                </a>
-                    <label id="Pseudo">user</label>
-                    <button type="button" class="btn btn-default" onclick="Start()">Deco</button>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#"> Bibliothèque Audio V2 </a>
+        </div>
+            <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right" >
+                 <label id="PseudoHeader">user</label>
+                 <input type="button" class="btn btn-default navbar-btn"  value="Déconnexion" onclick="Start()" />
+            </ul>
             </div>
         </div>
-    </nav>
+</nav>
