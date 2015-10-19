1195c17c6c9fddcc8fc1c871993f5d3f432168b4
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-19 23:26:52 +0200

    ajout des bouton admin et ajout gestion playlist

---
+++ View/GestionPlaylist.php
@@ -0,0 +1,29 @@
+<table class="table">
+    <thead>
+    <th>Nom</th>
+    <th>Nombre de titre</th>
+    <th>Ecouter</th>
+    <th></th>
+</thead>
+<tbody>
+    <tr>
+        <td>
+            La first
+        </td>
+        <td>
+            5
+        </td>
+        <td>
+            <audio controls="controls">
+
+            </audio>
+        </td>
+        <td>
+            <div class="btn-group" role="group" >
+                <button type="button" class="btn btn-default">Modifier</button>
+                <button type="button" class="btn btn-danger">Supprimer</button>
+            </div>
+        </td>
+    </tr>
+</tbody>
+</table>
--- View/MenuGauche.php
+++ View/MenuGauche.php
@@ -7,4 +7,14 @@
         <li <?php if ($_SERVER['PHP_SELF'] == "/BibliothequeAudio/view/playlist.php") echo "class='active'"; ?>  class="btn-info"><a href="#">Gestion du profile</a></li>
         <li <?php if ($_SERVER['PHP_SELF'] == "/BibliothequeAudio/view/Request_Audio.php") echo "class='active'"; ?>  class="btn-info"><a href="#"> Faire une demande</a></li>
     </ul>
+    <br/>
+    <h3>Administration</h3>
+    <br/>
+    <ul class="nav nav-divider nav-stacked">
+        <li <?php if ($_SERVER['PHP_SELF'] == "/BibliothequeAudio/view/playlist.php") echo "class='active'"; ?>  class="btn-info"><a href="#">Gestion des demandes</a></li>
+        <li <?php if ($_SERVER['PHP_SELF'] == "/BibliothequeAudio/view/Request_Audio.php") echo "class='active'"; ?>  class="btn-info"><a href="#"> Gestion des utilisateurs</a></li>
+        <li <?php if ($_SERVER['PHP_SELF'] == "/BibliothequeAudio/view/Request_Audio.php") echo "class='active'"; ?>  class="btn-info"><a href="#"> Gestion des Fichiers audio</a></li>
+    </ul>
+    
+   
 </div>
