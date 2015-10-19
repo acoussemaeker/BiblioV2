1a94691ba313469d63b7f229161ed1b6746a3693
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-19 19:48:37 +0200

    bon Positionnement de la page profile et changement de couleur de font

--- View/ProfilUser.php
+++ View/ProfilUser.php
@@ -1,20 +1,21 @@
-
 <div class="nouveau">
+    <br/>
+    <br/>
     <div class="milieuPage">
-      <div class="form-group">
+        <div class="form-group">
             <label for="contenu">Pseudo</label>
             <input type="text" placeholder="Pseudo" id="pseudo" class="form-control"/>
         </div>
-     
+
         <div class="form-group">
             <label for="contenu">Mot de passe</label>
             <input type="password" placeholder="Password" id="password" class="form-control"/>
         </div>
-        
+
         <div class="form-group">
             <label for="contenu">Mail</label>
             <input type="email" placeholder="Email" id="email" class="form-control"/>
         </div>
-       <input type="button" class="btn btn-success" id="Sauvegarder" value="Sauvegarder"  />
+        <input type="button" class="btn btn-success" id="Sauvegarder" value="Sauvegarder"  />
     </div>
 </div>
