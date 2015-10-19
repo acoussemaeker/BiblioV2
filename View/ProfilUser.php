feb594ce225c9e88822df8c61ae99c9e883bc441
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-19 15:10:48 +0200

    vue profile user

---
+++ View/ProfilUser.php
@@ -0,0 +1,20 @@
+
+<div class="nouveau">
+    <div class="milieuPage">
+      <div class="form-group">
+            <label for="contenu">Pseudo</label>
+            <input type="text" placeholder="Pseudo" id="pseudo" class="form-control"/>
+        </div>
+     
+        <div class="form-group">
+            <label for="contenu">Mot de passe</label>
+            <input type="password" placeholder="Password" id="password" class="form-control"/>
+        </div>
+        
+        <div class="form-group">
+            <label for="contenu">Mail</label>
+            <input type="email" placeholder="Email" id="email" class="form-control"/>
+        </div>
+       <input type="button" class="btn btn-success" id="Sauvegarder" value="Sauvegarder"  />
+    </div>
+</div>
