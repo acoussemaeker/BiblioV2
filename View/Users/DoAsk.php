9379125989b851e8ec7ddd4d39b3af86bf29e083
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-20 16:48:05 +0200

    ajout de la page demande pour l'utilisateur

---
+++ View/Users/DoAsk.php
@@ -0,0 +1,13 @@
+<div class="nouveau">
+    ici bientot tous ce qu'il faut pour ajouté des musique au biblio 
+    
+    en attendant :
+    <div class="form-control">
+        <label for="contenu">Titre :</label>
+        <input type="text" placeholder="Titre"/>
+    </div>    
+    <div class="form-control">
+        <input type="file" class="btn bg-primary" />
+    </div>
+    
+</div>
