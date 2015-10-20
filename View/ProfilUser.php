1d7a8e5f7b4d626b8cc9bbc6e211dc99dd05fade
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-20 08:31:23 +0200

    blocage des champ sur la page profilUser.php

--- View/ProfilUser.php
+++ View/ProfilUser.php
@@ -4,17 +4,17 @@
     <div class="milieuPage">
         <div class="form-group">
             <label for="contenu">Pseudo</label>
-            <input type="text" placeholder="Pseudo" id="Pseudo" class="form-control"/>
+            <input type="text" readonly placeholder="Pseudo" id="Pseudo" class="form-control"/>
         </div>
 
         <div class="form-group">
             <label for="contenu">Mot de passe</label>
-            <input type="password" placeholder="Password" id="Password" class="form-control"/>
+            <input type="password" readonly placeholder="Password" id="Password" class="form-control"/>
         </div>
 
         <div class="form-group">
             <label for="contenu">Mail</label>
-            <input type="email" placeholder="Email" id="Email" class="form-control"/>
+            <input type="email" readonly placeholder="Email" id="Email" class="form-control"/>
         </div>
         <input type="button" class="btn btn-default" id="Sauvegarder" value="Modifier" onclick="LoadModifProfilUser()" />
     </div>
