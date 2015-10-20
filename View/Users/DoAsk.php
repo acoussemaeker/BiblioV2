89d4350d6fe479d5269661c4f48121a888914a72
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-20 23:18:58 +0200

    refonte du style de la page doask

--- View/Users/DoAsk.php
+++ View/Users/DoAsk.php
@@ -1,13 +1,17 @@
 <div class="nouveau">
-    ici bientot tous ce qu'il faut pour ajouté des musique au biblio 
-    
-    en attendant :
-    <div class="form-control">
-        <label for="contenu">Titre :</label>
-        <input type="text" placeholder="Titre"/>
-    </div>    
-    <div class="form-control">
-        <input type="file" class="btn bg-primary" />
+    <div class="milieuPage">
+        <h2>Demander l'ajout d'une musique</h2>
+        <form>
+        <div class="form-group">
+            <label >Titre :</label>
+            <input type="text" class="form-control" placeholder="titre">
+        </div>
+        <div class="form-group">
+            <label>Fichier :</label>
+            <input type="file" class="btn btn-default" id="Fichier">
+            <p class="help-block">ajoutez le fichier de la musique que vous désirez</p>
+        </div>
+            <input type="button" value="Sauvegarder" class="btn btn-success"/>
+        </form>
     </div>
-    
 </div>
