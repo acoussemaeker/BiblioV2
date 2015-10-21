6a6a20ba8388dee65373f432f3f1da56b50cd4e3
acoussemaeker <arnaud.cousco@hotmail.fr>
2015-10-21 21:36:53 +0200

    virage des alerts

--- Js/Script.js
+++ Js/Script.js
@@ -555,7 +555,6 @@ function LaodDataPersonalLibrary(){
             $('#listLibrary').empty();
             for (var t in dataa) {
                 $('#ListLibrary').append("<tr><td>"+dataa[t].Emplacement+"</td><td>"+dataa[t].Nom+"</td><td><button onclick=\"startListener('"+dataa[t].Emplacement+"')\" type=\"button\"><span class=\"glyphicon glyphicon-play\" aria-hidden=\"true\"></span></button></td> <td><div class=\"btn-group\" role=\"group\" ><input type=\"button\" class=\"btn btn-danger\" value=\"Supprimer de la Bibliotheque personnel\" onclick=\"DeleteAudioUser("+dataa[t].Id+")\"/> </div> </td> </tr>");
-                alert(dataa[t].Emplacement);
             }
         },
         error: function (XMLHttpRequest, textStatus, errorThrows) { // erreur durant la requete
@@ -620,7 +619,6 @@ function LoadDoAskView(){
 }
 
 function startListener(data) {
-    alert(data);
     var newaudio = "<audio id=\"audio\" class=\"listener\" controls=\"controls\" buffered preload=\"none\"> <source src=\"audio/"+data+"\" type=\"audio/mp3\" />Votre navigateur n'est pas compatible </audio>";
     $("#listener").empty();
     $("#listener").append(newaudio);         
